1bd8a4fe4c0002f544faeba831c57ebb40afae05
Brijesh Kumar <bk10895@gmail.com>
2024-10-22 16:49:51 +0530

    Update package dependencies and add react-iconsRefactor CSS and update App component, add Footer and Layout components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import { FaRegUser } from 'react-icons/fa6';
+import { IoMdNotificationsOutline } from 'react-icons/io';
+import Link from 'next/link';
 
 export const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-      <p>Some text</p>
-    </div>
+    <header className="flex items-center justify-between bg-[#2f405b] p-4 shadow-md">
+      <img
+        src="https://images.squarespace-cdn.com/content/v1/5aafcd3a4eddeccf69eae084/50316180-ada6-4bc7-ba98-5fb825380184/White+logo+with+lockup+icon+3.png?format=1500w"
+        alt="Logo"
+        className="mr-4 h-[50px] w-[167px] md:h-[53px] md:w-[175px]"
+      />
+      <div className="flex items-center">
+        <nav className="hidden space-x-6 md:flex">
+          <Link href="/" className="text-white hover:text-blue-600">
+            Home
+          </Link>
+          <Link href="/explore" className="text-white hover:text-blue-600">
+            Explore
+          </Link>
+          <Link href="/projects" className="text-white hover:text-blue-600">
+            Projects
+          </Link>
+        </nav>
+      </div>
+      <div className="flex items-center space-x-4">
+        <input
+          type="text"
+          className="rounded border p-2"
+          placeholder="Search..."
+        />
+        {/* Render the notification icon directly */}
+        <IoMdNotificationsOutline className="h-6 w-6 text-white" />
+        <div className="relative">
+          <FaRegUser className="h-6 w-6 text-white" />
+        </div>
+      </div>
+    </header>
   );
 };
