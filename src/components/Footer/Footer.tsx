2bc7d7b609b41ddd372ee7e7637fa2630ba1f9b1
Brijesh Kumar <bk10895@gmail.com>
2024-10-22 22:33:04 +0530

    Refactor Footer component to use react-icons for social media links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,12 @@
+import { FaInstagram, FaLinkedin, FaXTwitter } from 'react-icons/fa6';
+
 import Link from 'next/link';
 
 export const Footer = () => {
   return (
     <footer className="text-white mt-10 bg-gray-100 p-4">
       <div className="mx-auto flex max-w-7xl justify-between">
-        <div className="space-y-2">
+        <div className="space-x-4">
           <Link href="/terms" className="text-white-100 hover:underline">
             Terms & Conditions
           </Link>
@@ -12,10 +14,16 @@ export const Footer = () => {
             Privacy Policy
           </Link>
         </div>
-        <div className="flex space-x-4">
-          <img src="/linkedin.svg" alt="LinkedIn" className="h-6 w-6" />
-          <img src="/twitter.svg" alt="Twitter" className="h-6 w-6" />
-          <img src="/instagram.svg" alt="Instagram" className="h-6 w-6" />
+        <div className="flex gap-2 space-x-4">
+          <Link href="https://www.instagram.com" className="text-white-100">
+            <FaInstagram className="h-6 w-6" />
+          </Link>
+          <Link href="https://www.linkedin.com" className="text-white-100">
+            <FaLinkedin className="h-6 w-6" />
+          </Link>
+          <Link href="https://www.twitter.com" className="text-white-100">
+            <FaXTwitter className="h-6 w-6" />
+          </Link>
         </div>
       </div>
     </footer>
